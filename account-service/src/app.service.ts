7a86b0287dea268987e96fe1c3b6574d3a981d45
Jonas Stjerne <jost@trifork.com>
2023-11-12 17:45:28 +0100

    Add prisma

--- account-service/src/app.service.ts
+++ account-service/src/app.service.ts
@@ -5,4 +5,16 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getBalance() {
+    return 100;
+  }
+
+  transfer({ toUser, amount }: { toUser: any; amount: number }) {
+    return { data: 'Success' };
+  }
+
+  deposit(amount: number) {}
+
+  withdraw(amount: number) {}
 }
